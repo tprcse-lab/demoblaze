68a71cbbb7341f621c45bb3bb324ee3c76b415f5
sasikumar <sasipsk3689@gmail.com>
2025-11-28 15:35:48 +0530

    add cart to chart

---
+++ src/test/java/demoblaze/addtocart.java
@@ -0,0 +1,10 @@
+package demoblaze;
+
+public class addtocart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Addto cart mobile phone");
+	}
+
+}
